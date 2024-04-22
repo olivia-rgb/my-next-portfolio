dd0828e1a269e2bdc25e5f56ebf98710196d02aa
olivia <oliviaonyinyechiokafor@gmail.com>
2024-04-23 00:49:11 +0100

    another change

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import {Providers} from "./providers";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,10 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Providers>
-        
         {children}
-      </Providers>
         </body>
     </html>
   )
